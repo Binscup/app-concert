4f339705f44a53b28efaf104a225bfbc182ee9ab
Vincent Rahadian Utama <vincentrahadianutama>
2023-03-20 10:03:32 +0700

    pembaruan contact

--- login/login.js
+++ login/login.js
@@ -2,13 +2,13 @@ function Login(){
     let user = document.getElementById("username").value
     let pass = document.getElementById("password").value
 
-    if (user=="zaldi zaldi","binscup","husain","muh cs" && pass=="123"){
+    if (user=="zaldi zaldi","binscup","husain","muh cs" && pass=="123456"){
         alert("Selamat anda berhasil login")
-    }else if (user != "zaldi zaldi","binscup","husain" && pass == "123"){
+    }else if (user != "zaldi zaldi","binscup","husain","muh cs" && pass == "123456"){
         alert("Username tidak terdaftar!")
-    }else if(user=="zaldi zaldi","binscup","husain","muh cs" && pass != "123"){
-        alert("password salah!")
-    }else if(user != "zaldi zaldi","binscup","husain"&& pass != "123"){
+    }else if(user == "zaldi zaldi","binscup","husain","muh cs" && pass != "123456"){
+        alert("Password salah!")
+    }else if(user != "zaldi zaldi","binscup","husain","muh cs" && pass != "123456"){
         alert("Anda adalah orang yang tidak dikenal. pergi dari situs ini!")
     }
 }
